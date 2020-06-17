de9df9582e09628de2b85f277723e63b1b5815d8
Thanachot007 <46196885+Thanachot007@users.noreply.github.com>
2020-06-17 08:57:07 +0700

    models folder

---
+++ models/employeeModel.js
@@ -0,0 +1,13 @@
+var mongoose = require("mongoose");
+var empSchema = mongoose.Schema({
+    fname: { type: String, required: true },
+    lname: { type: String, required: true },
+    email: { type: String, required: true },
+    tel: { type: String, required: true, min: 0, max: 10 },
+    userName: { type: String, required: true },
+    password: { type: String, required: true }
+
+})
+
+var empModel = mongoose.model("employees", empSchema);
+module.exports = empModel
